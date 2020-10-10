f541674d22b4e5c34ea3b5759a0b11ca6576c1c8
stefba <st@rg-s.com>
2020-10-10 23:12:33 +0200

    Polish text component

--- app/src/components/types/text.js
+++ app/src/components/types/text.js
@@ -26,36 +26,37 @@ const Text = ({file, moveFn, delFn, single}) => {
     }
   }, [single]);
 
-  const handleTyping = event => {
-    setBody(event.target.value);
+  const handleTyping = evt => {
+    setBody(evt.target.value);
   }
 
-  const submit = event => {
-    fetch("/api" + file.path, {
-      method: "POST",
-      body:   body
-    })
-    .catch((error) => {
-      console.error('Error:', error);
-    });
-  }
-
-  let minRows = 1;
-
-  if (single) {
-    minRows = Math.round(window.screen.height/(2.25*16)) - 8;
+  const submit = async evt => {
+    try {
+      await fetch("/api" + file.path, {
+        method: "POST",
+        body:   body
+      });
+    } catch(err) {
+      alert(err);
+    }
   }
 
   return (
     <>
-      { !single && <Info file={file} moveFn={moveFn} delFn={delFn} /> }
+    { !single &&
+      <Info file={file} moveFn={moveFn} delFn={delFn} />
+    }
       <TextareaAutosize value={body}
-      ref={ref}
-      minRows={minRows}
-      onChange={handleTyping}
-      onBlur={submit} />
+        ref={ref}
+        minRows={single ? 1 : fullScreenRows()}
+        onChange={handleTyping}
+        onBlur={submit} />
     </>
   )
+}
+
+function fullScreenRows() {
+  return Math.round(window.screen.height/(2.25*16)) - 8
 }
 
 export default Text;
