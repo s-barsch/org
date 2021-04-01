2ce3fed6b6deed854bc60692ab157a17fb4fedcb
stefba <st@rg-s.com>
2021-04-01 12:34:35 +0200

    Add global hotkeys

--- src/components/main/views/dir.tsx
+++ src/components/main/views/dir.tsx
@@ -6,6 +6,7 @@ import { DirList, FileList } from 'components/main/parts/list';
 import { AddDir } from 'components/main/parts/add';
 import { navObj, errObj } from 'app';
 import Nav from 'components/nav/nav';
+import { HotKeys } from "react-hotkeys";
 
 type DirViewProps = {
     path: string;
@@ -16,9 +17,18 @@ type DirViewProps = {
     nav: navObj;
 }
 
+
 export default function DirView({path, files, mainFuncs, modFuncs, nav, err}: DirViewProps) {
+const keyMap = {
+    NEW_TEXT: "ctrl+enter"
+};
+
+const handlers = {
+    NEW_TEXT: mainFuncs.createNewFile
+};
+
     return (
-        <>
+        <HotKeys keyMap={keyMap} handlers={handlers}>
             <Nav pathname={path} newFile={mainFuncs.createNewFile} nav={nav} err={err} />
             <Head path={path} renameView={mainFuncs.renameView} />
             <nav id="dirs">
@@ -31,7 +41,7 @@ export default function DirView({path, files, mainFuncs, modFuncs, nav, err}: Di
                    */ }
                 <FileList files={filesOnly(files)} mainFuncs={mainFuncs} modFuncs={modFuncs} saveSort={mainFuncs.saveSort} />
             </section>
-        </>
+        </HotKeys>
     )
 }
 
