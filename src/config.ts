42f59351272e1181a7cd59bcb6ee2b304b8db232
stefba <st@rg-s.com>
2022-10-20 22:13:00 +0200

    Update config

--- src/config.ts
+++ src/config.ts
@@ -5,12 +5,9 @@ type config = {
 let Config: config = {
     links: [
     "/search",
-    "/topics",
     "/write",
     "/today",
     "/public/graph",
-    "/public/log",
-    "/public/index",
     "/public/kine",
     "/public/about"
 ]}
